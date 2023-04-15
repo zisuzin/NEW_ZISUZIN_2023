0120c20e1594ff5a87121877cfdfa510d9c0adee
zisuzin <zi_soo_zin@naver.com>
2023-04-15 20:26:02 +0900

    s

--- project/02.webApp/js/autoslide.js
+++ project/02.webApp/js/autoslide.js
@@ -1,12 +1,11 @@
-function autoSlide() {}
-export default autoSlide {
-    let swiper = new Swiper('.swiper', {
-        direction: 'vertical',
-        loop: true,
-      
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-    });
-}
+/* *****************************
+	* SNS 이미지 드래그 슬라이드
+	* swiper.js 필요
+***************************** */
+let swiper = new Swiper(".swiper-wrapper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
