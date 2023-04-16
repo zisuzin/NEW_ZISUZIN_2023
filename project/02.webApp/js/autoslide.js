30e6fe3eb7c8a73a134480b3ee12597cfc456e56
zisuzin <zi_soo_zin@naver.com>
2023-04-16 20:01:38 +0900

    s

--- project/02.webApp/js/autoslide.js
+++ project/02.webApp/js/autoslide.js
@@ -2,7 +2,8 @@
 	* SNS 이미지 드래그 슬라이드
 	* swiper.js 필요
 ***************************** */
-  let swiper = new Swiper(".swiper-container", {
+  /* (공통) 상단 우측 배너 */
+  new Swiper(".swiper", {
       slidesPerView: 1,
       spaceBetween: 0,
       loop: true, // 무한루프(기본값:false)
@@ -19,3 +20,40 @@
       },
       slidesPerGroup: 1, // 슬라이드 그룹(개수단위로 넘어감!)
     });
+
+
+  /* 1. 메인 컨텐츠1 */
+  new Swiper(".swiper2", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true, // 무한루프(기본값:false)
+    pagination: {
+      // 블릿설정
+      el: ".pg-nav2", // 블릿요소설정
+      clickable: true, // 클릭가능여부
+    },
+    navigation : { // 네비게이션 설정
+      nextEl : '.swiper-button-next', // 다음 버튼 클래스명
+      prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
+    },
+    // autoplay: {
+    //   // 자동넘김설정
+    //   delay: 4000, // 시간간격(1/1000초)
+    //   disableOnInteraction: false,
+    //   // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
+    // },
+    slidesPerGroup: 1, // 슬라이드 그룹(개수단위로 넘어감!)
+
+
+  });
+
+  const pgBtn = document.querySelectorAll(".pgBtn");
+  const pgcont1 = document.querySelector(".section1_container");
+  
+  pgcont1.addEventListener("mouseover", ()=>{
+  pgBtn.forEach(ele => {
+    ele.classList.toggle("showBtn");
+      console.log("콘솔!")
+    });
+  });
+
