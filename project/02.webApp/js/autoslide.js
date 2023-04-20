c7b1a135d3b45227a438b752bc9406a7761caf02
zisuzin <zi_soo_zin@naver.com>
2023-04-21 02:22:29 +0900

    s

--- project/02.webApp/js/autoslide.js
+++ project/02.webApp/js/autoslide.js
@@ -3,27 +3,27 @@
 	* swiper.js 필요
 ***************************** */
   /* (공통) 상단 우측 배너 */
-  new Swiper(".swiper", {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      loop: true, // 무한루프(기본값:false)
-      pagination: {
-        // 블릿설정
-        el: ".swiper-pagination", // 블릿요소설정
-        clickable: true, // 클릭가능여부
-      },
-      autoplay: {
-        // 자동넘김설정
-        delay: 4000, // 시간간격(1/1000초)
-        disableOnInteraction: false,
-        // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
-      },
-      slidesPerGroup: 1, // 슬라이드 그룹(개수단위로 넘어감!)
-  });
+  // new Swiper(".swiper", {
+  //     slidesPerView: 1,
+  //     spaceBetween: 0,
+  //     loop: true, // 무한루프(기본값:false)
+  //     pagination: {
+  //       // 블릿설정
+  //       el: ".swiper-pagination", // 블릿요소설정
+  //       clickable: true, // 클릭가능여부
+  //     },
+  //     autoplay: {
+  //       // 자동넘김설정
+  //       delay: 4000, // 시간간격(1/1000초)
+  //       disableOnInteraction: false,
+  //       // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
+  //     },
+  //     slidesPerGroup: 1, // 슬라이드 그룹(개수단위로 넘어감!)
+  // });
 
 
   /* 메인 컨텐츠1 */
-  new Swiper(".swiper2", {
+  new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true, // 무한루프(기본값:false)
@@ -36,37 +36,14 @@
       nextEl : '.swiper-button-next', // 다음 버튼 클래스명
       prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
     },
-    // autoplay: {
-    //   // 자동넘김설정
-    //   delay: 4000, // 시간간격(1/1000초)
-    //   disableOnInteraction: false,
-    //   // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
-    // },
+    autoplay: {
+      // 자동넘김설정
+      delay: 4000, // 시간간격(1/1000초)
+      disableOnInteraction: false,
+      // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
+    },
     slidesPerGroup: 1, // 슬라이드 그룹(개수단위로 넘어감!)
 
 
   });
 
-  /* 메인 컨텐츠1
-    - 이벤트대상 영역에 마우스호버/리브시 버튼스타일 변경 */
-  function showBtn () {
-    const LBtn = document.querySelector(".swiper-button-prev");
-    const RBtn = document.querySelector(".swiper-button-next");
-    const pgcont1 = document.querySelector(".section1_container");
-    
-    pgcont1.addEventListener("mouseenter", ()=>{
-      LBtn.style.opacity = "1";
-      RBtn.style.opacity = "1";
-      LBtn.classList.add("on");
-      RBtn.classList.add("on");
-      });
-  
-    pgcont1.addEventListener("mouseleave", ()=>{
-      LBtn.style.opacity = "0";
-      RBtn.style.opacity = "0";
-      LBtn.classList.remove("on");
-      RBtn.classList.remove("on");
-    });
-  }
-
-  showBtn();
