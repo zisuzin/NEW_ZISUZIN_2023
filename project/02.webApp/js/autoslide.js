4a14b71df285a89bca478534281e564d2b4fab5a
zisuzin <zi_soo_zin@naver.com>
2023-04-21 15:35:35 +0900

    s

--- project/02.webApp/js/autoslide.js
+++ project/02.webApp/js/autoslide.js
@@ -29,7 +29,7 @@
   /* 메인 컨텐츠2 */
   new Swiper(".swiper2", {
     slidesPerView: 3,
-    spaceBetween: 0,
+    spaceBetween: 20,
     loop: true, // 무한루프(기본값:false)
     // navigation : { // 네비게이션 설정
     //   nextEl : '.swiper-button-next', // 다음 버튼 클래스명
