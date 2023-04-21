1864fdc41e00f7c1aa5539f661df01dbea64c78f
zisuzin <zi_soo_zin@naver.com>
2023-04-21 17:17:31 +0900

    s

--- project/02.webApp/js/autoslide.js
+++ project/02.webApp/js/autoslide.js
@@ -31,16 +31,16 @@
     slidesPerView: 3,
     spaceBetween: 20,
     loop: true, // 무한루프(기본값:false)
-    // navigation : { // 네비게이션 설정
-    //   nextEl : '.swiper-button-next', // 다음 버튼 클래스명
-    //   prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
-    // },
+    navigation : { // 네비게이션 설정
+      nextEl : '.main-navBtnL', // 다음 버튼 클래스명
+      prevEl : '.main-navBtnR', // 이번 버튼 클래스명
+    },
     // autoplay: {
     //   // 자동넘김설정
     //   delay: 4000, // 시간간격(1/1000초)
     //   disableOnInteraction: false,
     //   // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
     // },
-    // slidesPerGroup: 3, // 슬라이드 그룹(개수단위로 넘어감!)
+    slidesPerGroup: 3, // 슬라이드 그룹(개수단위로 넘어감!)
   });
 
