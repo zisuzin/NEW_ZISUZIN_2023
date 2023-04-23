abb9a8d53631a946e5c269f39942e8e98b3cece2
zisuzin <zi_soo_zin@naver.com>
2023-04-24 01:48:41 +0900

    s

--- project/02.webApp/js/autoslide.js
+++ project/02.webApp/js/autoslide.js
@@ -8,11 +8,6 @@
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true, // 무한루프(기본값:false)
-    pagination: {
-      // 블릿설정
-      el: ".pg-nav2", // 블릿요소설정
-      clickable: true, // 클릭가능여부
-    },
     navigation : { // 네비게이션 설정
       nextEl : '.swiper-button-next', // 다음 버튼 클래스명
       prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
@@ -33,14 +28,32 @@
     loop: true, // 무한루프(기본값:false)
     navigation : { // 네비게이션 설정
       nextEl : '.main-navBtnL', // 다음 버튼 클래스명
-      prevEl : '.main-navBtnR', // 이번 버튼 클래스명
+      prevEl : '.main-navBtnN', // 이번 버튼 클래스명
     },
-    // autoplay: {
-    //   // 자동넘김설정
-    //   delay: 4000, // 시간간격(1/1000초)
-    //   disableOnInteraction: false,
-    //   // 상호작용(건드리는 것!)이 없으면 다시 재시작(false일때)
-    // },
+    slidesPerGroup: 3, // 슬라이드 그룹(개수단위로 넘어감!)
+  });
+
+  /* 메인 컨텐츠3 */
+  new Swiper(".swiper3", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true, // 무한루프(기본값:false)
+    navigation : { // 네비게이션 설정
+      nextEl : '.main3-navBtnL', // 다음 버튼 클래스명
+      prevEl : '.main3-navBtnN', // 이번 버튼 클래스명
+    },
+    slidesPerGroup: 3, // 슬라이드 그룹(개수단위로 넘어감!)
+  });
+
+  /* 메인 컨텐츠4 */
+  new Swiper(".swiper4", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true, // 무한루프(기본값:false)
+    navigation : { // 네비게이션 설정
+      nextEl : '.main4-navBtnL', // 다음 버튼 클래스명
+      prevEl : '.main4-navBtnN', // 이번 버튼 클래스명
+    },
     slidesPerGroup: 3, // 슬라이드 그룹(개수단위로 넘어감!)
   });
 
