bacf435e7d046fcb2e7f3c6a8cd1417afcd4c24b
zisuzin <zi_soo_zin@naver.com>
2023-04-25 23:15:32 +0900

    s

---
+++ project/02.webApp/js/goBtn.js
@@ -0,0 +1,22 @@
+// 버튼클릭시 상단이동
+const mTopBtn = document.querySelector(".scrollTop");
+
+    mTopBtn.addEventListener("click",()=>{
+        window.scrollTo({
+            top:0,
+            behavior:'smooth'
+        });
+    });
+
+// 상단이동시 숨기기/보이기
+window.addEventListener("scroll", ()=>{
+    let scVal = window.scrollY;
+    console.log("scVal",scVal);
+    if(scVal === 0) {
+        mTopBtn.style.opacity = 0;
+        mTopBtn.classList.add("active");
+    } else {
+        mTopBtn.style.opacity = 1;
+        mTopBtn.classList.remove("active");
+    }
+});
