f87d129ea1a8d0edd05c705caba179c51c3bce8f
zisuzin <zi_soo_zin@naver.com>
2023-05-01 02:18:43 +0900

    ㄴ

--- project/02.webApp/js/switch_screens.js
+++ project/02.webApp/js/switch_screens.js
@@ -3,30 +3,29 @@ const arrow = document.querySelector("svg");
 
 // fadeOut 애니 완료 여부 저장 변수
 let isScrolling = false;
-let AniComplete = false;
+let AniComplete = false; 
 
-// 화살표애니 완료후 실행 함수 
+// 화살표애니 완료후 실행 함수
 function handleAniComplete(){
-  AniComplete = true;
-  if(isScrolling && AniComplete) { 
-    window.location.href = "main.html";
-  }
+    AniComplete = true;
+    if(isScrolling && AniComplete) { // 화살표애니 완료되고, 스크롤 발생시 실행
+        window.location.href = "main.html";
+    }
 }
 
-// 스크롤이벤트감지 함수
+// 스크롤이벤트 감지 함수
 function handleScrollEvt(){
-  if(!isScrolling) { 
-    isScrolling = true; 
-    fadeContbx.classList.add('fade-out');
-    setTimeout(() => {
-        // 사용후에는 대상 스크롤이 아니므로 제거
-        window.removeEventListener("scroll", handleScrollEvt);
-        // 페이지이동 후 불필요하므로 제거
-        arrow.removeEventListener("animationend", handleAniComplete); 
-        window.location.href = "main.html";
-    }, 100);
-  }
+    if(!isScrolling) { 
+        isScrolling = true;
+        fadeContbx.classList.add('fade-out');
+        setTimeout(() => {
+            window.removeEventListener("scroll", handleScrollEvt);
+            arrow.removeEventListener("animationend", handleAniComplete);
+            handleAniComplete();
+        }, 100);
+    }
 }
 
 window.addEventListener("scroll", handleScrollEvt);
-arrow.addEventListener("animationend", handleAniComplete);
+arrow.addEventListener("animationend", handleAniComplete); 
+
