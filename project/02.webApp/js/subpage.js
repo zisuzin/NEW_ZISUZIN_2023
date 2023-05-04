a413cfaf0e2e7f1c89cb68e72292091089f4f2ae
zisuzin <zi_soo_zin@naver.com>
2023-05-04 16:57:08 +0900

    s

--- project/02.webApp/js/subpage.js
+++ project/02.webApp/js/subpage.js
@@ -1,24 +1,64 @@
 import sdata from "./tempData/data-sub.js";
 
-// ###### 메인영역 메뉴 뷰 템플릿 셋팅하기 #######
+// ##### 컴포넌트 등록 #### 
+// 2. 메인영역
+// 메인영역1 메뉴 뷰 템플릿 셋팅
 Vue.component("stab-comp", {
-  name: "stab-comp",
-  template: sdata.new_best_tab,
+    name: "stab-comp",
+    template: sdata.new_best_tab,
 });
 
-// ###### 하단영역 메뉴 뷰 템플릿 셋팅하기 #######
-// Vue.component("prod-comp", {
-//   name: "prod-comp",
-//   template: sdata.prod_list,
-// });
+// 메인영역2 상품리스트 뷰 템플릿 셋팅
+Vue.component("list-comp", {
+    name: "list-comp",
+    template: sdata.prod_list,
+});
 
-// sdata 변수 정의는 Vue 인스턴스 생성 코드 밑으로 이동합니다.
+// 3. 하단영역
+// 하단영역 뷰 템플릿 셋팅
 
-//###### 메인영역 뷰 인스턴스 생성하기 ##########
+// ##### 뷰 인스턴스 생성 #####
+// 1. 상단영역
+
+// 2. 메인영역
+// 숫자증감 변수
+let num = 0;
+
 new Vue({
-  el: ".new-prod-container",
-  data: {},
-  mounted: function () {
-    console.log("mounted구역");
-  },
+    el: ".new_container",
+    data() {
+      return {
+        isrc:`../00.data/02.imgData/new_sc_comp/item_${this.setNum()}.jpg `,
+        sub_tit1: "NEW",
+        sub_tit2: "BEST",
+          products: [
+            {
+              brand: "슈펜키즈",
+              name: "키즈 피셔맨 말랑 젤리샌들 HPKV1D204U",
+              price: 59900,
+            },
+            {
+              brand: "슈펜키즈",
+              name: "키즈 피셔맨 말랑 피치샌들 HPKV1D204U",
+              price: 59900,
+            },
+            {
+              brand: "슈펜키즈",
+              name: "키즈 피셔맨 말랑 젤리샌들 HPKV1D204U",
+              price: 59900,
+            },
+            {
+              brand: "슈펜키즈",
+              name: "키즈 피셔맨 말랑 젤리샌들 HPKV1D204U",
+              price: 59900,
+            },
+          ],
+        };
+    }, 
+    methods: {
+      setNum() {
+          num+=1;
+          return num;
+        }
+    }
 });
