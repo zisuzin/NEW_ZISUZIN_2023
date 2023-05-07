e419eb039df2c045289b485129ca813890e038f4
zisuzin <zi_soo_zin@naver.com>
2023-05-07 21:56:39 +0900

    s

--- project/02.webApp/js/left_moving_slider.js
+++ project/02.webApp/js/left_moving_slider.js
@@ -1,7 +1,34 @@
 function moveFn() {
-  const slideWrap = document.querySelector(".swiper-wrapper");
+  const slidewrap = $(".new-item-list");
+  let sc_sts = 0;
   let lpos = 0;
+  let call_sts = 1;
+  let callT;
+  const startTop = slidewrap.offset().top - $(window).height();
 
-  const tgH = slideWrap.getBoundingClientRect().top;
-  console.log(tgH)
-}
+  function moveList() {
+    lpos--;
+    if(lpos < -262) {
+      lpos = 0;
+      slidewrap.append(slidewrap.find("li").first());
+    }
+
+    slidewrap.css({
+      left: lpos + "px",
+    });
+
+    if(call_sts) callT = setTimeout(moveList, 40);
+  }
+
+  slidewrap.hover(
+    function(){
+      call_sts = 0;
+    }
+    ,function(){
+      call_sts = 1;
+      moveList();
+    }
+  );
+} 
+
+moveFn();
