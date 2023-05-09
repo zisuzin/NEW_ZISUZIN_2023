95dcabd7648ccb6a286bb86db309e89d814e8fc0
zisuzin <zi_soo_zin@naver.com>
2023-05-09 10:40:11 +0900

    s

--- project/02.webApp/js/subpage.js
+++ project/02.webApp/js/subpage.js
@@ -10,54 +10,104 @@ import matchData from "./tempData/subData.js";
 // });
 
 // 메인영역2 상품리스트 뷰 템플릿 셋팅
-Vue.component("list-comp", {
-  template: `
-      <li>
-        <div class="new-prod-bx">
-            <a href="#">
-                <div class="prod-img">
-                    <img v-bind:src="pimg" alt="상품이미지">
-                </div>
-                <div class="prod-txt">
-                    <strong class="brand">슈펜키즈</strong>
-                    <p>{{pname}}</p>
-                </div>
-            </a>
-        </div>
-        <div class="item-detail">
-            <span class="original-price">
-                <em>{{poprice}}</em>
-                <span>원</span>
-            </span>
-            <br>
-            <span class="discount-price">
-                <em>{{pdprice}}</em>
-                <span>원</span>
-            </span>
-            <span class="percent-price">
-                <em>{{psale}}</em>
-                <span>%</span>
-            </span>
-        </div>
-      </li>
-    `,
-    props: ["matchItem"],
-    data: function() {
-      return {
-        pname: this.matchItem.name,
-        pimg: `./00.data/02.imgData/new_sc_comp/${this.matchItem.img}.jpg" alt="상품이미지"`,
-        poprice: this.matchItem.oprice,
-        pdprice: this.matchItem.dprice,
-        psale: this.matchItem.sale,
-      }
-    },
-});
-
 new Vue({
     el: ".new_container",
     data: {
       matchData: matchData,
     }, 
-    mounted: function() {}
+    components: {
+      props: ["matchItem"],
+      data: function() {
+        return {
+          pname: this.matchItem.name,
+          pimg: `./00.data/02.imgData/new_sc_comp/${this.matchItem.img}.jpg" alt="상품이미지"`,
+          poprice: this.matchItem.oprice,
+          pdprice: this.matchItem.dprice,
+          psale: this.matchItem.sale,
+        }
+      },
+      "list-comp":{
+          template: `
+          <li>
+          <div class="new-prod-bx">
+              <a href="#">
+                  <div class="prod-img">
+                      <img v-bind:src="this.pimg" alt="상품이미지">
+                  </div>
+                  <div class="prod-txt">
+                      <strong class="brand">슈펜키즈</strong>
+                      <p>{{this.pname}}</p>
+                  </div>
+              </a>
+          </div>
+          <div class="item-detail">
+              <span class="original-price">
+                  <em>{{this.poprice}}</em>
+                  <span>원</span>
+              </span>
+              <br>
+              <span class="discount-price">
+                  <em>{{this.pdprice}}</em>
+                  <span>원</span>
+              </span>
+              <span class="percent-price">
+                  <em>{{this.psale}}</em>
+                  <span>%</span>
+              </span>
+          </div>
+        </li>
+          `
+      } 
+    }
 });
+// Vue.component("list-comp", {
+//   template: `
+//       <li>
+//         <div class="new-prod-bx">
+//             <a href="#">
+//                 <div class="prod-img">
+//                     <img v-bind:src="pimg" alt="상품이미지">
+//                 </div>
+//                 <div class="prod-txt">
+//                     <strong class="brand">슈펜키즈</strong>
+//                     <p>{{pname}}</p>
+//                 </div>
+//             </a>
+//         </div>
+//         <div class="item-detail">
+//             <span class="original-price">
+//                 <em>{{poprice}}</em>
+//                 <span>원</span>
+//             </span>
+//             <br>
+//             <span class="discount-price">
+//                 <em>{{pdprice}}</em>
+//                 <span>원</span>
+//             </span>
+//             <span class="percent-price">
+//                 <em>{{psale}}</em>
+//                 <span>%</span>
+//             </span>
+//         </div>
+//       </li>
+//     `,
+//     props: ["matchItem"],
+//     data: function() {
+//       return {
+//         pname: this.matchItem.name,
+//         pimg: `./00.data/02.imgData/new_sc_comp/${this.matchItem.img}.jpg" alt="상품이미지"`,
+//         poprice: this.matchItem.oprice,
+//         pdprice: this.matchItem.dprice,
+//         psale: this.matchItem.sale,
+//       }
+//     },
+// });
+
+// new Vue({
+//     el: ".new_container",
+//     data: {
+//       matchData: matchData,
+//     }, 
+//     mounted: function() {}
+// });
 
