49d98f90473e582aa354cde81c003b3609eda09c
zisuzin <zi_soo_zin@naver.com>
2023-05-10 11:23:34 +0900

    s

--- project/02.webApp/js/common.js
+++ project/02.webApp/js/common.js
@@ -2,16 +2,19 @@ import comData from "./tempData/commonData.js"
 
 // 상단영역 뷰 컴포넌트
 Vue.component("top-area",{
+    name: "top-area",
     template: comData.topArea
 });
 
 // 메인영역 뷰 컴포넌트
 Vue.component("view_prod_bx",{
+    name: "view_prod_bx",
     template: comData.rcent_view_pbx
 });
 
 // 하단영역 뷰 컴포넌트
 Vue.component("info-area",{
+    name: "info-area",
     template: comData.infoArea
 });
 
