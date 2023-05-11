f4a68d8d20a3a5d9dfc2a3db406da97996f4b4e7
zisuzin <zi_soo_zin@naver.com>
2023-05-11 16:10:13 +0900

    s

--- project/02.webApp/js/tempData/subData.js
+++ project/02.webApp/js/tempData/subData.js
@@ -19,30 +19,30 @@ const subdata = {
     `,
 
     prodList: `
-        <li v-for="(value,name) in $store.state.newData.전체.item">
+        <li v-for="(value,name) in $store.state.newData">
             <div class="new-prod-bx">
                 <a href="#">
-                    <div class="prod-img">
-                        <img v-bind:src="'./00.data/02.imgData/'+value.cat+value.item['img']+.jpg'" alt="상품이미지">
+                    <div class="prod-img" v-for="(a,b) in value.item">
+                        <img v-bind:src="'./00.data/02.imgData/new/' + value.cat + a.img+'.jpg'" alt="상품이미지">
                     </div>
                     <div class="prod-txt">
                         <strong class="brand">슈펜</strong>
-                        <p>{{value['name']}}</p>
+                        <p>{{value.name}}</p>
                     </div>
                 </a>
             </div>
             <div class="item-detail">
                 <span class="original-price">
-                    <em>{{value['oprice']}}</em>
-                    <span v-if="value['oprice']">원</span>
+                    <em>{{value.oprice}}</em>
+                    <span v-if="value.oprice">원</span>
                 </span>
                 <br>
                 <span class="discount-price">
-                    <em>{{value['dprice']}}</em>
-                    <span v-if="value['dprice']">원</span>
+                    <em>{{value.dprice}}</em>
+                    <span v-if="value.dprice">원</span>
                 </span>
-                <span class="percent-price" v-if="value['sale']">
-                    <em>{{value['sale']}}</em>
+                <span class="percent-price" v-if="value.sale">
+                    <em>{{value.sale}}</em>
                 </span>
             </div>
         </li>
