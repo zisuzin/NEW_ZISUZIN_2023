3a91a7aae09b18e64184683bcc3180c5d9aab3e2
zisuzin <zi_soo_zin@naver.com>
2023-05-14 13:10:54 +0900

    여성 서브페이지

--- project/02.webApp/js/subpage.js
+++ project/02.webApp/js/subpage.js
@@ -1,6 +1,5 @@
 import subdata from "./tempData/subData_nb.js";
-import msubdata from "./tempData/subData_wmk.js"
-import comData from "./tempData/commonData.js"
+// import msubdata from "./tempData/subData_wmk.js"
 import store from "./store.js";
 
 // ##### 컴포넌트 등록 #### 
@@ -13,9 +12,9 @@ Vue.component("stab-comp", {
         store.commit('newChgData',category)
         // this.chgData(category);
         // this.$store.commit("showMoreItems"); // showMoreItems 뮤테이션 호출
-        if (category === '전체') {
-          this.$store.commit('showMoreItems');
-        }
+        // if (category === '전체') {
+        //   this.$store.commit('showMoreItems');
+        // }
       },
     },
     mounted(){
@@ -28,46 +27,42 @@ Vue.component("stab-comp", {
     },
 });
 
-Vue.component("subTab-comp",{
-  template: msubdata.sideTab,
-})
-
 new Vue({
   el: "#cont_wrap",
   store, // 뷰엑스 스토어 등록
   data: {
     subTit: ["NEW","BEST"],
   }, 
-  component: {
+  components: {
     // 서브페이지 상품리스트 뷰 템플릿 셋팅
     "list-comp": {
-      name: list-comp,
-      template: subdata.new_prodList,
-      methods: {
-      // 할인률 계산 - ((정가 - 할인가) / 정가) * 100 
-      calculateDiscount(oprice,dprice){
-        if(!oprice || !dprice) {
-          return "";
-        }
-        const discount = ((oprice - dprice) / oprice) * 100;
-        return Math.floor(discount) + '%'
-      },
-      // 정규식함수(숫자 세자리마다 콤마해주는 기능)
-      numberWithCommas(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-      },
-      chgData(param) {
-        store.state.setsNum = store.state.newData[param].sNum;
-        store.state.setcat = store.state.newData[param].cat;
-        store.state.setitem = store.state.newData[param].item;
-      }
+        template: subdata.new_prodList,
+        methods: {
+          // 할인률 계산 - ((정가 - 할인가) / 정가) * 100 
+          calculateDiscount(oprice,dprice){
+            if(!oprice || !dprice) {
+              return "";
+            }
+            const discount = ((oprice - dprice) / oprice) * 100;
+            return Math.floor(discount) + '%'
+          },
+          // 정규식함수(숫자 세자리마다 콤마해주는 기능)
+          numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+          },
+          chgData(param) {
+            store.state.setsNum = store.state.newData[param].sNum;
+            store.state.setcat = store.state.newData[param].cat;
+            store.state.setitem = store.state.newData[param].item;
+          }
+        },
+        mounted(){
+          // console.log(store.state.newData['전체'].item)
+        },
     },
-    mounted(){
-      // console.log(store.state.newData['전체'].item)
-      },
-    }
   }
-})
+}); // Vue 인스턴스
+
 
 // 메뉴 뷰 템플릿 클릭시 스타일 적용
 const subTab = document.querySelectorAll(".new-prod-tab > ul > li");
--- project/02.webApp/js/tempData/subData_wmk.js
+++ project/02.webApp/js/tempData/subData_wmk.js
@@ -1,132 +1,132 @@
-const msubdata = {
-    // 상품 카테고리 선택 Tab메뉴
-    sideTab: `
-        <aside class="cat_left_side_tab">
-            <h2 class="tit_h2">WOMEN</h2>
-            <div class="ctg_link">
-            <ul>
-                <li>
-                <a href="#">신상
-                    <em>NEW</em>
-                </a>
-                </li>
-                <li>
-                <a href="#">베스트
-                    <em>BEST</em>
-                </a>
-                </li>
-            </ul>
-            </div>
-            <div class="ctg_nav">
-            <ul>
-                <li class="ctg_depth1">
-                <div>
-                    <a href="#">
-                    <span>여성신발</span>
-                    </a>
-                </div>
-                <div class="ctg_depth2">
-                    <ul>
-                    <li>
-                        <a href="#">전체</a>
-                    </li>
-                    <li>
-                        <a href="#">플랫/단화</a>
-                    </li>
-                    <li>
-                        <a href="#">샌들</a>
-                    </li>
-                    <li>
-                        <a href="#">스니커즈/운동화</a>
-                    </li>
-                    </ul>
-                </div>
-                </li>
-                <li class="ctg_depth1">
-                <div>
-                    <a href="#">
-                    <span>여성가방</span>
-                    </a>
-                </div>
-                <div class="ctg_depth2">
-                    <ul>
-                    <li>
-                        <a href="#">전체</a>
-                    </li>
-                    <li>
-                        <a href="#">백팩</a>
-                    </li>
-                    <li>
-                        <a href="#">미니백</a>
-                    </li>
-                    <li>
-                        <a href="#">크로스백</a>
-                    </li>
-                    </ul>
-                </div>
-                </li>
-                <li class="ctg_depth1">
-                <div>
-                    <a href="#">
-                    <span>여성잡화</span>
-                    </a>
-                </div>
-                <div class="ctg_depth2">
-                    <ul>
-                    <li>
-                        <a href="#">전체</a>
-                    </li>
-                    <li>
-                        <a href="#">양말</a>
-                    </li>
-                    <li>
-                        <a href="#">모자</a>
-                    </li>
-                    <li>
-                        <a href="#">머플러/장갑</a>
-                    </li>
-                    </ul>
-                </div>
-                </li>
-            </ul>
-            </div>
-        </aside>
-    `,
+// const msubdata = {
+//     // 상품 카테고리 선택 Tab메뉴
+//     sideTab: `
+//         <aside class="cat_left_side_tab">
+//             <h2 class="tit_h2">WOMEN</h2>
+//             <div class="ctg_link">
+//             <ul>
+//                 <li>
+//                 <a href="#">신상
+//                     <em>NEW</em>
+//                 </a>
+//                 </li>
+//                 <li>
+//                 <a href="#">베스트
+//                     <em>BEST</em>
+//                 </a>
+//                 </li>
+//             </ul>
+//             </div>
+//             <div class="ctg_nav">
+//             <ul>
+//                 <li class="ctg_depth1">
+//                 <div>
+//                     <a href="#">
+//                     <span>여성신발</span>
+//                     </a>
+//                 </div>
+//                 <div class="ctg_depth2">
+//                     <ul>
+//                     <li>
+//                         <a href="#">전체</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">플랫/단화</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">샌들</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">스니커즈/운동화</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 </li>
+//                 <li class="ctg_depth1">
+//                 <div>
+//                     <a href="#">
+//                     <span>여성가방</span>
+//                     </a>
+//                 </div>
+//                 <div class="ctg_depth2">
+//                     <ul>
+//                     <li>
+//                         <a href="#">전체</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">백팩</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">미니백</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">크로스백</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 </li>
+//                 <li class="ctg_depth1">
+//                 <div>
+//                     <a href="#">
+//                     <span>여성잡화</span>
+//                     </a>
+//                 </div>
+//                 <div class="ctg_depth2">
+//                     <ul>
+//                     <li>
+//                         <a href="#">전체</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">양말</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">모자</a>
+//                     </li>
+//                     <li>
+//                         <a href="#">머플러/장갑</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 </li>
+//             </ul>
+//             </div>
+//         </aside>
+//     `,
 
-    // 상품 정보 리스트(NEW)
-    tg_prodList: `
-        <ul>
-            <li v-for="(value,name) in $store.state.newData[$store.state.setcat].item" :key="name">
-                <div>
-                    <div class="new-prod-bx">
-                        <a href="#">
-                            <div class="prod-img">
-                                <img v-bind:src="'./00.data/02.imgData/new/'+ $store.state.cat + '/' + value.img + '.jpg'" alt="상품이미지">
-                            </div>
-                            <div class="prod-txt">
-                                <strong class="brand">슈펜</strong>
-                                <p>{{value.name}}</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="item-detail">
-                        <span class="original-price">
-                            <em>{{numberWithCommas(value.oprice)}}</em>
-                            <span v-if="value.oprice">원</span>
-                        </span>
-                        <br>
-                        <span class="discount-price">
-                            <em>{{numberWithCommas(value.dprice)}}</em>
-                            <span v-if="value.dprice">원</span>
-                        </span>
-                        <span class="percent-price" v-if="value.oprice && value.dprice">
-                            <em>{{calculateDiscount(value.oprice, value.dprice)}}</em>
-                        </span>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    `,
-}
+//     // 상품 정보 리스트(NEW)
+//     tg_prodList: `
+//         <ul>
+//             <li v-for="(value,name) in $store.state.newData[$store.state.setcat].item" :key="name">
+//                 <div>
+//                     <div class="new-prod-bx">
+//                         <a href="#">
+//                             <div class="prod-img">
+//                                 <img v-bind:src="'./00.data/02.imgData/new/'+ $store.state.cat + '/' + value.img + '.jpg'" alt="상품이미지">
+//                             </div>
+//                             <div class="prod-txt">
+//                                 <strong class="brand">슈펜</strong>
+//                                 <p>{{value.name}}</p>
+//                             </div>
+//                         </a>
+//                     </div>
+//                     <div class="item-detail">
+//                         <span class="original-price">
+//                             <em>{{numberWithCommas(value.oprice)}}</em>
+//                             <span v-if="value.oprice">원</span>
+//                         </span>
+//                         <br>
+//                         <span class="discount-price">
+//                             <em>{{numberWithCommas(value.dprice)}}</em>
+//                             <span v-if="value.dprice">원</span>
+//                         </span>
+//                         <span class="percent-price" v-if="value.oprice && value.dprice">
+//                             <em>{{calculateDiscount(value.oprice, value.dprice)}}</em>
+//                         </span>
+//                     </div>
+//                 </div>
+//             </li>
+//         </ul>
+//     `,
+// }
 
-export default msubdata;
+// export default msubdata;
