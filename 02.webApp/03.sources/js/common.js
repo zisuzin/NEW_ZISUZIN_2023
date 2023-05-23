970a0c521f69534c894a958cf060032e15d8667b
zisuzin <zi_soo_zin@naver.com>
2023-05-24 01:28:47 +0900

    new new2

--- 02.webApp/03.sources/js/common.js
+++ 02.webApp/03.sources/js/common.js
@@ -1,26 +1,28 @@
-import comData from "./tempData/commonData.js"
-import store from "./store.js";
-// 상단영역 뷰 컴포넌트
-Vue.component("top-area",{
-    name: "top-area",
-    template: comData.topArea,
-});
+// 공통 기능 JS
+function catToggle(){
+    // 카테고리메뉴 클릭시 토글
+    $(".ctg_depth1 > div").click(function () {
+      const $target = $(this).parent(".ctg_depth1").find(".ctg_depth2");
+  
+      // 이미 열려있는 ctg_depth2 요소 닫음
+      if ($target.is(":visible")) {
+        $target.slideUp(300);
+        $(this).removeClass("slide-down");
+      }
+      else {
+        // 다른 ctg_depth2 요소를 모두 닫음
+        $(".ctg_depth2").slideUp(300);
+        $(".ctg_depth1 > div").removeClass("slide-down");
+  
+        // 현재 클릭한 요소를 토글
+        $target.slideToggle(300);
+        $(this).toggleClass("slide-down");
+      }
+  
+      $('.ctg_depth2 > ul > li').on('click', function(event) {
+        event.stopPropagation();
+      });
+    });
+} // catToggle 함수
 
-// 하단영역 뷰 컴포넌트
-Vue.component("info-area",{
-    name: "info-area",
-    template: comData.infoArea,
-});
-
-Vue.component("prod-comp",{
-    template: comData.rcent_view_pbx,
-});
-
-const makeVue = x => new Vue({el:x});
-makeVue("#info");
-makeVue(".recent-view-prod-bx");
-
-new Vue({
-    el: "#header",
-    store,
-})
+export { catToggle };
