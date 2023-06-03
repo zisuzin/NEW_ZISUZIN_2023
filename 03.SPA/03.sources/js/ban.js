e06069fae0ae925d05de17e0633b07f3addc755c
zisuzin <zi_soo_zin@naver.com>
2023-06-03 16:20:06 +0900

    ss

--- 03.SPA/03.sources/js/ban.js
+++ 03.SPA/03.sources/js/ban.js
@@ -1,19 +1,30 @@
 // PC - 휠이벤트 + 메인배너이동
 
+import {qs, qsa} from "./eventFn.js";
+
 /****************************************** 
     함수명: handleWheel
     기능: 휠 이동시 슬라이드 위치 변경
 ******************************************/
+let prot = 0;
+
 function handleWheel(e){
+
+    // 광휠금지
+    if(prot) return;
+    prot = 1;
+    setTimeout(()=>{
+        prot = 0
+    },100);
+
     // 슬라이드 겉박스
-    const moveSl = $(".bancont")
-    console.log(moveSl)
+    const moveSl = qs(".album_wrap");
+    
     // 대상 슬라이드 li
-    const tgsl = $(".album_set")
+    const tgsl = qsa(".album_set");
 
     // 휠 방향 체크
     const delta = -e.wheelDelta;
-    // console.log("휠방향:",delta);
 
     // 윈도우 높이값
     const winH = window.innerHeight;
@@ -24,17 +35,22 @@ function handleWheel(e){
     console.log("문서높이:",docH) 
     // 휠 한계값
 
-    const currentTop = parseInt(moveSl.css("top"));
+    const currentTop = parseInt(getComputedStyle(moveSl).top);
     // delta값이 양수인 경우 (아래로 휠-> 위로 이동)
     if(delta > 0) {
-        // console.log(currentTop)
-        moveSl.css("top", currentTop - delta + "px");
-        // const lastLi = moveSl.find("ul > li").last();
+        moveSl.style.top = currentTop - delta + "px";
+        moveSl.appendChild(moveSl.firstChild);
     }
     // delta값이 음수인 경우 (위로 휠-> 아래로 이동) 
     else if (delta < 0) {
-        moveSl.css("top", currentTop - delta + "px");
+        moveSl.insertBefore(tgsl[6], moveSl.firstChild)
+        moveSl.style.top = currentTop - delta + "px";
     }
+
+    tgsl.forEach((ele, idx) => {
+        // data-seq 라는 사용자정의 속성 넣기
+        ele.setAttribute("data-seq", idx);
+    }); // forEach //
 
 }
 
---
+++ 03.SPA/03.sources/js/eventFn.js
@@ -0,0 +1,5 @@
+// 요소 선택 함수
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
+
+export {qs, qsa};
