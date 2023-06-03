fdafe936d4f7204c17fc12037580444a5f763024
zisuzin <zi_soo_zin@naver.com>
2023-06-03 19:18:31 +0900

    ss

--- 03.SPA/03.sources/js/ban.js
+++ 03.SPA/03.sources/js/ban.js
@@ -27,15 +27,6 @@ function handleWheel(e){
     // 휠 방향 체크
     const delta = -e.wheelDelta;
 
-    // 윈도우 높이값
-    const winH = window.innerHeight;
-    console.log("윈도우높이:",winH) 
-
-    // 전체문서 높이값
-    const docH = document.body.offsetHeight;
-    console.log("문서높이:",docH) 
-    // 휠 한계값
-
     // delta값이 양수인 경우 (아래로 휠-> 위로 이동)
     tgsl.forEach((ele, idx) => {
         if(delta > 0) {
@@ -44,7 +35,7 @@ function handleWheel(e){
         }
         // delta값이 음수인 경우 (위로 휠-> 아래로 이동) 
         else if (delta < 0) {
-            ele.style.top = `-${ele.offsetHeight}px`;
+            ele.style.top = `+${ele.offsetHeight}px`;
             moveSl.insertBefore(tgsl[6], moveSl.firstChild)
         }
 
