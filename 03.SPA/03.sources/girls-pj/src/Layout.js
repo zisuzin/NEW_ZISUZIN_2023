6ea3ca6221ac004454c20d927c637820a97f9fce
zisuzin <zi_soo_zin@naver.com>
2023-06-10 21:34:46 +0900

    gnb 메뉴토글러 구현

--- 03.SPA/03.sources/girls-pj/src/Layout.js
+++ 03.SPA/03.sources/girls-pj/src/Layout.js
@@ -9,10 +9,8 @@ function handleToggle() {
   const nav = document.querySelector(".circular_nav");
   const toggleBtn = nav.querySelector(".toggle_btn");
 
-  toggleBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-    toggleBtn.classList.toggle("active");
-  });
+  nav.classList.toggle("active");
+  toggleBtn.classList.toggle("active");
 }
 
 const Layout = () => {
@@ -23,35 +21,35 @@ const Layout = () => {
       <nav className="circular_nav">
         <div className="nav_content">
           {/* 메뉴토글 버튼 */}
-          <div className="toggle_btn">
+          <div className="toggle_btn" onClick={handleToggle}>
             <i className="bx bx-plus"></i>
           </div>
           {/* 1. 홈버튼 */}
-          <span>
+          <span style={{"--i":1}}>
             <Link to="/">
               <i className="bx bxs-home"></i>
             </Link>
           </span>
           {/* 2. 갤러리버튼 */}
-          <span>
+          <span style={{"--i":2}}>
             <Link to="/ga">
               <i className="bx bxs-camera"></i>
             </Link>
           </span>
           {/* 3. 비디오버튼 */}
-          <span>
+          <span style={{"--i":3}}>
             <Link to="/vd">
               <i className="bx bxs-video"></i>
             </Link>
           </span>
           {/* 4. 앨범버튼 */}
-          <span>
+          <span style={{"--i":4}}>
             <Link to="/al">
               <i className="bx bxs-music"></i>
             </Link>
           </span>
           {/* 5. 프로필버튼 */}
-          <span>
+          <span style={{"--i":5}}>
             <Link to="/pf">
               <i className="bx bxs-user-rectangle"></i>
             </Link>
