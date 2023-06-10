7cf331df548014ffe14b79fa830a45e5f284e258
zisuzin <zi_soo_zin@naver.com>
2023-06-11 01:07:54 +0900

    ss

--- 03.SPA/03.sources/girls-pj/src/components/Player.js
+++ 03.SPA/03.sources/girls-pj/src/components/Player.js
@@ -1,87 +1,23 @@
 // 플레이어 컴포넌트 - Player.js
+import React, { useState } from "react";
 // 플레이어CSS
 import "../scss/player.css";
 // 배너 데이터
 import ban_data from "../data/ban";
 import $ from "jquery";
 
-/****************************************** 
-    함수명: player
-    기능: 
-    (1) 슬라이드li 2번째 요소에 맞춰 플레이어 음원/커버 변경
-    (2) 오디오 볼륨조절
-    (3) 음원 현재 재생시간/ 총 재생시간 업데이트
-    (4) 플레이버튼 토글하여 음원 재생/멈춤
-******************************************/
-function handlePlayer() {
-  const audio = $("#music")[0];
-  const play_btn = $(".play_song_btn");
-  const elapsed = $("#elapsed");
-  const total = $("#total_timer");
-  const current = $("#progress_timer");
+// 제이쿼리 로드구역 함수 /////////
+function jqFn() {
+    $(() => {}); //////// jQB ///////////
+} ////////////// jQFn ///////////
 
-  // 오디오 재생/멈춤버튼 토글
-  play_btn.click(function () {
-    play_btn.toggleClass("active");
-    /* active 클래스 가지고있으면 이미지 변경! */
-    if (play_btn.hasClass("active")) {
-      play_btn.find("img").attr("src", "./images/player/bx-pause.svg");
-    } else {
-      play_btn.find("img").attr("src", "./images/player/bx-play-circle.svg");
-    }
-  });
-
-  // 오디오의 재생 시간이 업데이트될 때마다 함수 호출!
-  audio.addEventListener("timeupdate", function () {
-    // timeupdate - 오디오 또는 비디오의 재생 시간이 변경될 때마다 발생하는 이벤트
-    // 오디오의 재생 시간과 전체 재생 시간 가져오기
-    const currentTime = audio.currentTime;
-    const duration = audio.duration; // 오디오 총 길이
-    // 음원 총 길이 구해서 시간 업데이트
-    const minutes = Math.floor(duration / 60);
-    const seconds = Math.floor(duration % 60);
-    const totalTime = minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0"); // 자료형을 문자화한 후 앞에 "0"을 추가!
-    total.text(totalTime);
-
-    // 음원 재생시간 구해서 시간 업데이트
-    const minutes2 = Math.floor(currentTime / 60);
-    const seconds2 = Math.floor(currentTime % 60);
-    const currentTime2 = minutes2.toString().padStart(2, "0") + ":" + seconds2.toString().padStart(2, "0"); // 자료형을 문자화한 후 앞에 "0"을 추가!
-    current.text(currentTime2);
-
-    // 프로그레스 바의 너비 계산
-    const progress = (currentTime / duration) * 100 + "%";
-    // console.log("프로그레스바너비:",progress)
-
-    // 프로그레스 바 업데이트
-    elapsed.css("width", progress);
-  });
-
-  play_btn.click(function () {
-    if (audio.paused) {
-      // 재생버튼 클릭시 음원 재생
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  });
-}
-
-function handleVolume() {
-  const volumeBar = $("#volume");
-  const audio = $("#music")[0];
-  // 오디오 볼륨 조절
-  // 볼륨 조절바를 조작할때 이벤트 발생!
-  const volumeVal = $(this).val(); // 오디오 요소 - 현재 볼륨값을 값으로 가져옴
-  audio.volume = volumeVal;
-}
 
 // 플레이어 출력용 컴포넌트
 function Player() {
   return (
     <>
       {ban_data.map((x, i) => (
-        <div className="player" key={i}>
+        <div className="player" key={i}> 
           <span id="arm"></span>
           <ul>
             <li className="artwork">
@@ -124,7 +60,7 @@ function Player() {
                   </span>
                   <div className="slider">
                     <div className="volume"></div>
-                    <input type="range" id="volume" min="0" max="1" step="0.01" value="0.6" onChange={handleVolume} />
+                    <input type="range" id="volume" min="0" max="1" step="0.01"/>
                   </div>
                 </div>
               </div>
@@ -132,6 +68,8 @@ function Player() {
           </ul>
         </div>
       ))}
+      {/* 빈루트를 만들고 JS로드함수포함 */}
+      {jqFn()}
     </>
   );
 }
