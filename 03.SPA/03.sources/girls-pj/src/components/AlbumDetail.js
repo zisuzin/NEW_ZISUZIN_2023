7c03f28445b257523cb86e4c3f6672ebda3f4085
zisuzin <zi_soo_zin@naver.com>
2023-06-13 23:06:50 +0900

    앨범리스트 클릭시 각각의 페이지로 이동

--- 03.SPA/03.sources/girls-pj/src/components/AlbumDetail.js
+++ 03.SPA/03.sources/girls-pj/src/components/AlbumDetail.js
@@ -1,28 +1,30 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Album_Ban } from "./Ban";
 import ban_data from "../data/ban";
 
 function AlbumDetail () {
   // 반환된 객체에서 각각의 id 속성을 추출!
   const { id } = useParams();
 
+  // 각각의 id순번 데이터에 접근하기 위한 변수
   const eachAlbum = ban_data.main[id];
-//   console.log(ban_data.main[id])
 
   return (
     <>
-      {/* <h2>Album Detail - {id}</h2> */}
-      <main className="container">
-        <div className="contents_wrap">
+      <main>
+        <div className="albumlist_wrap">
           {/* 1. 좌측 컨텐츠 */}
           <div className="contents_left">
             {/* 1-1. 뒤로가기 버튼 */}
             <div className="back_btn">
+              <Link to={<Album_Ban/>}>
               <span>＜BACK</span>
+              </Link>
             </div>
             {/* 1-2. 앨범 커버 이미지 */}
             <div className="album_cover">
-              <img src={eachAlbum.isrc} alt={'여자아이들의 '+ eachAlbum.mtit +' 앨범입니다.'} />
+              <img src={'.'+eachAlbum.isrc} alt={'여자아이들의 '+ eachAlbum.mtit +' 앨범'} />
             </div>
             {/* 1-3. 앨범 발매일 */}
             <div className="release_date">
