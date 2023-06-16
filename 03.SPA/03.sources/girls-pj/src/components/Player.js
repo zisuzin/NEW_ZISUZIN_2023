e8fca4f83c5032569f223eebda47fcac3ad776a0
zisuzin <zi_soo_zin@naver.com>
2023-06-16 20:09:03 +0900

    다음버튼 클릭시 다음곡 재생

--- 03.SPA/03.sources/girls-pj/src/components/Player.js
+++ 03.SPA/03.sources/girls-pj/src/components/Player.js
@@ -1,6 +1,7 @@
 // 플레이어 컴포넌트 - Player.js
 import React, { useRef, useState, useEffect } from "react";
-import AudioPlayer from 'react-audio-player';
+// 오디오 재생/컨트롤 라이브러리
+import { Howl } from "howler";
 // 플레이어CSS
 import "../scss/player.css";
 // 배너 데이터
@@ -57,12 +58,12 @@ function handleplayer() {
     requestAnimationFrame(stopLp);
   }
 
-    // 오디오 볼륨 조절
-    // volumeBar.on("change", function() { // 볼륨 조절바를 조작할때 이벤트 발생!
-    //   const volumeVal = $(this).val(); // 오디오 요소 - 현재 볼륨값을 값으로 가져옴
-    //   console.log(volumeVal)
-    //   // audio.volume = volumeVal;
-    // })
+  // 오디오 볼륨 조절
+  // volumeBar.on("change", function() { // 볼륨 조절바를 조작할때 이벤트 발생!
+  //   const volumeVal = $(this).val(); // 오디오 요소 - 현재 볼륨값을 값으로 가져옴
+  //   console.log(volumeVal)
+  //   // audio.volume = volumeVal;
+  // })
 } // handleplayer 함수
 
 $(document).ready(function () {
@@ -73,7 +74,7 @@ $(document).ready(function () {
 function Player(props) {
   const audioPlayer = useRef(null);
   const sel_data = ban_data[props.cat];
-  const [volume, setVolume] = useState(1);  
+  const [volume, setVolume] = useState(1);
 
   const handleVolumeChange = (event) => {
     const volumeValue = event.target.value;
@@ -84,71 +85,88 @@ function Player(props) {
     // console.log(audioPlayer.current.volume)
     const audioElement = audioPlayer.current;
     audioElement.volume = volumeValue;
-  setVolume(volumeValue);
+    setVolume(volumeValue);
   };
 
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const soundRef = useRef(null);
+  
+  // 다음 버튼 클릭시 다음 곡 재생 
+  const playNextSong = () => {
+    const nextIndex = (currentSongIndex + 1) % sel_data.length;
+    setCurrentSongIndex(nextIndex);
+    playAudio(sel_data[nextIndex].vsrc);
+  };
+
+  // Howl 라이브러리(이전/다음 곡 컨트롤)
+  const playAudio = (audioSrc) => {
+    if (soundRef.current) {
+      soundRef.current.stop();
+    }
+    
+    const sound = new Howl({
+      src: [audioSrc],
+      html5: true,
+      onend: playNextSong, 
+    });
+    sound.play();
+    soundRef.current = sound; // 현재 재생 중인 곡을 참조
+  };
+
+  const x = ban_data.main[currentSongIndex];
+  console.log(x)
+
   return (
-    <>
-    <div>
-      <AudioPlayer
-        src="path/to/your/audio/file.mp3" // 오디오 파일 경로
-        autoPlay // 자동 재생 설정
-        controls // 플레이어 컨트롤러 표시
-      />
-    </div>
-      {/* {sel_data.map((x, i) => (
-        <div className={"player msp" + (i + 1)} key={i}>
-          <span id="arm"></span>
-          <ul>
-            <li className="artwork">
-              <a href="#">
-                <img src={x.isrc} alt="레코드이미지" />
-              </a>
-            </li>
-            <li className="info"> */}
-              {/* 곡 정보 */}
-              {/* <h1 id="album">{x.mtit.replace(/-/g, "")}</h1>
-              <h4 id="artist">(G)I-DLe</h4>
-              <h2 id="song">{x.mtit}</h2>
-              <div className="button-items">
-                <audio id="music" ref={audioPlayer}>
-                  <source src={x.vsrc} type="audio/mp3"/>
-                </audio> */}
-                {/* 음반 리스트 */}
-                {/* <div id="slider"> */}
-                  {/* 음원 재생시간 프로그레스바 */}
-                  {/* <div id="elapsed"></div>
-                  <div id="buffered"></div>
-                </div> */}
-                {/* 현재 재생시간 */}
-                {/* <p id="progress_timer" className="timer">
-                  00:00
-                </p> */}
-                {/* 총 재생시간 */}
-                {/* <p id="total_timer" className="timer">
-                  00:00
-                </p>
-                <div className="controls">
-                  <span className="prev_song_btn">
-                    <i className="bx bx-skip-previous"></i>
-                  </span>
-                  <span className="play_song_btn">
-                    <img src={"./images/player/bx-play-circle.svg"} alt="정지버튼" />
-                  </span>
-                  <span className="next_song_btn">
-                    <i className="bx bx-skip-next"></i>
-                  </span>
-                  <div className="slider">
-                    <div className="volume"></div>
-                    <input type="range" id="volume" min={0} max={1} step={0.01} value={volume} onChange={handleVolumeChange}/>
-                  </div>
-                </div>
+    <div className={"player"}>
+      <span id="arm"></span>
+      <ul>
+        <li className="artwork">
+          <a href="#">
+            <img src={x.isrc} alt="레코드이미지" />
+          </a>
+        </li>
+        <li className="info">
+          {/* 곡 정보 */}
+          <h1 id="album">{x.mtit.replace(/-/g, "")}</h1>
+          <h4 id="artist">(G)I-DLe</h4>
+          <h2 id="song">{x.mtit}</h2>
+          <div className="button-items">
+            <audio id="music">
+              <source src={x.vsrc} type="audio/mp3" />
+            </audio>
+            {/* 음반 리스트 */}
+            <div id="slider">
+              {/* 음원 재생시간 프로그레스바 */}
+              <div id="elapsed"></div>
+              <div id="buffered"></div>
+            </div>
+            {/* 현재 재생시간 */}
+            <p id="progress_timer" className="timer">
+              00:00
+            </p>
+            {/* 총 재생시간 */}
+            <p id="total_timer" className="timer">
+              00:00
+            </p>
+            <div className="controls">
+              <span className="prev_song_btn">
+                <i className="bx bx-skip-previous"></i>
+              </span>
+              <span className="play_song_btn">
+                <img src={"./images/player/bx-play-circle.svg"} alt="정지버튼" />
+              </span>
+              <span className="next_song_btn" onClick={playNextSong}>
+                <i className="bx bx-skip-next"></i>
+              </span>
+              <div className="slider">
+                <div className="volume"></div>
+                <input type="range" id="volume" min={0} max={100} step={1} value={volume} onChange={handleVolumeChange} />
               </div>
-            </li>
-          </ul>
-        </div>
-      ))} */}
-    </>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 } // Player 함수
 
