ef6b978f88d6f4e06a2d3799144a150e0c414488
zisuzin <zi_soo_zin@naver.com>
2023-07-07 17:37:34 +0900

    배너 넘김시 텍스트 등장 효과

--- 02.webApp/03.sources/js/main.js
+++ 02.webApp/03.sources/js/main.js
@@ -60,7 +60,7 @@ function slideFn() {
 
         // 슬라이드 left위치값
         let sleft = $(this).offset().left;
-        console.log("left위치값:",sleft)
+        // console.log("left위치값:",sleft)
 
         // 분기점 : -110%보다 작을때
         if (sleft < -winW*1.1) {
@@ -76,6 +76,8 @@ function slideFn() {
             addOn(2);
             // 영상 재생함수 호출!
             vidOn(2);
+            // 배너텍스트 변경함수 호출!
+            showTit(2);
         } ///// if : 왼쪽이동 /////
 
         // 분기점 : -90%보다 클때
@@ -93,6 +95,8 @@ function slideFn() {
             addOn(0);
             // 영상 재생함수 호출!
             vidOn(0);
+            // 배너텍스트 변경함수 호출!
+            showTit(0);
         }  ///// else if : 오른쪽이동 /////
 
         // 분기점 : 제자리일경우
@@ -114,7 +118,7 @@ function slideFn() {
         // 배너 li
         const list = slide.find("li");
         const cnt = list.length;
-        console.log("슬라이드개수:",cnt)
+        // console.log("슬라이드개수:",cnt)
         
         // 조건문
         list.each((idx,ele) => {
@@ -139,6 +143,7 @@ function addOn(seq) { // seq - 읽을 슬라이드
     // 0 - 오른쪽, 2 - 왼쪽 이동
     // 슬라이드 data-seq 읽어오기
     let getseq = slide.find("li").eq(seq).attr("data-seq");
+    console.log("현재순번:",getseq)
 
     // 해당 슬라이드와 동일순번 블릿에 클래스 on
     indic.eq(getseq).addClass("on")
@@ -161,6 +166,7 @@ function vidOn(seq) {
     let noneTg = tgVid.parent().siblings().find("video");
     // 출력대상
     let chgVar = indic.eq(getseq).find(".bid");
+    // 출력제외
     let noneVar = chgVar.parent().siblings().find(".bid");
 
     // 현재 비디오재생
@@ -173,7 +179,7 @@ function vidOn(seq) {
         tg.get(0).currentTime = 0;
         tg.get(0).play();
     }
-
+    
     function stop(tg) {
         tg.get(0).pause();
         // 재생시간 초기화
@@ -201,11 +207,15 @@ function vidOn(seq) {
     let countNum = setInterval(callback,100);
     // 콜백함수
     function callback() {
+        // 비디오 현재 재생시간 변수
         nowTime = tgVid.get(0).currentTime;
+        // 비디오 총 재생시간 변수
         maxTime = tgVid.get(0).duration;
         // console.log(nowTime,maxTime)
 
         timer = (100 * nowTime / maxTime);
+        // console.log(timer)
+
         // 출력
         chgVar.css({width: 3+timer+"%", transition: ".3s linear"})
 
@@ -217,7 +227,7 @@ function vidOn(seq) {
 
             // 광드래그 방지 커버 보이기
             cover.show();
-            // 슬라이드 이동
+            // 왼쪽으로 슬라이드 이동
             slide.animate({
                 left: -winW*2 + "px"
             }, 1000, "easeOutQuint", ()=>{
@@ -231,6 +241,8 @@ function vidOn(seq) {
             addOn(2);
             // 영상 재생함수 호출!
             vidOn(2);
+            // 배너텍스트 변경함수 호출!
+            showTit(2);
             // 한계값시 쌓임방지 인터벌초기화
             clearInterval(countNum);
         } /////// if ////////////
@@ -238,10 +250,88 @@ function vidOn(seq) {
         // 쌓임방지 인터벌초기화
         clearInterval(countNum);
     } //////////// callback 함수 ///////////
+
    } ////////////////////// vidTimer 함수 ///////////////////////
 } ////////////////////// vidOn 함수 //////////////////////
 
+/************************************************** 
+    배너 타이틀 데이터 셋팅!
+**************************************************/
+///////////////////////////////////////
+////// 각 배너 등장 타이틀 셋팅 /////////
+///////////////////////////////////////
+const mainTxtData = {
+    "ban1": "Skin Care",
+    "ban2": "Recycling",
+    "ban3": "Quality",
+}; /////////// mainTxtData 객체 ///////////
+
+const subTxtData = {
+    "ban1": "자연 에너지를 한 병에 담은 시그니처 테라피<br>이제 집에서도 손쉽게 아로마테라피를 즐겨보세요.",
+    "ban2": "자원의 재순환을 위한 제로웨이스트<br>폐도자기를 수거해 새로운 가치를 더합니다.",
+    "ban3": "유기농 비건 뷰티 브랜드<br>차별화된 프리미엄 원료를 피부로 직접 느껴보세요.",
+}; /////////// subTxtData 객체 ///////////
+
+function showTit(seq) {
+
+    // 대표배너
+    const mainBan = slide.find("li").eq(seq);
+    // 클래스명 읽어오기
+    let clsName = mainBan.attr("class");
+    console.log("클래스명:",clsName)
+    // 클래스명에 해당하는 객체값 읽어오기
+    let mainTxt = mainTxtData[clsName];
+    let subTxt = subTxtData[clsName];
+    // console.log("메인txt명:",mainTxt,"\n서브txt명:",subTxt);
+
+    // 1. 요소 추가하기
+    mainBan.append(`
+    <span class="txtbx">
+        <h2 class="btit"></h2>
+        <p class="stit"></p>
+    </span>
+    `);
+
+    // 2. 배너넣기
+    // 2-1. 공통박스 설정
+    mainBan.find(".txtbx").css({
+        position: "absolute",
+        left: "min(5vw,100px)",
+        bottom: "12vh",
+        zIndex: "10",
+        whiteSpace: "nowrap",
+        opacity: "0" // 처음에 투명
+    })
+    .animate({
+        bottom: "15vh",
+        opacity: "1",
+    },1000);
+
+    // 2-2. .btit / .stit 셋업
+    // .btit
+    mainBan.find(".btit").html(mainTxt)
+    .css({
+        font: "min(10vw,100px) var(--aromatica-font-secondary)",
+        color: "#fff",
+        marginBottom: "20px",
+        textShadow: "1px 1px 3px rgb(64 64 64)",
+    });
+    
+    mainBan.find(".stit").html(subTxt)
+    .css({
+        fontFamily: "var(--aromatica-font-primary)",
+        fontSize: "min(3vw,18px)",
+        fontWeight: "300",
+        lineHeight: "1.8",
+        color: "#fff",
+        textShadow: "1px 1px 3px rgb(64 64 64)",
+    });
+
+
+} //////// showTit 함수 ///////
+
 } //////// slideFn 함수 ///////
+
 
 // 전체 싸고있는 함수 최초호출!
 slideFn();
